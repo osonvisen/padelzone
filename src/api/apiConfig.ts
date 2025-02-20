767b07c763253f60ce0a50ff3d4ca592c458fa96
Ørjan S <osonvisen@gmail.com>
2025-02-20 12:54:07 +0100

    Lagt til en login-modal, forbedret login og register

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,4 +1,4 @@
 const apiUrl = "https://crudcrud.com/api/";
-const apiKey = "df38dd5f161a486180bba000d1927ceb";
+const apiKey = "17670ac7eda6449ab8c4884d12cf3e68";
 
 export const API_BASE_URL = apiUrl + apiKey;
