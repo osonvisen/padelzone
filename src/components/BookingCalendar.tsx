c3563749ce8f9e2a8d83b965c1e9ec49266f6c01
Ørjan S <osonvisen@gmail.com>
2025-02-26 18:43:42 +0100

    Laget funksjoner og sjekker for kalenderfunksjonen

--- src/components/BookingCalendar.tsx
+++ src/components/BookingCalendar.tsx
@@ -1,9 +1,30 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 
 const BookingCalendar = () => {
     const [selectedDate, setSelectedDate] = useState<string>(
         new Date().toISOString().split("T")[0]
     );
+    // Vi skal velge dato og få opp oversikten over ledige tidspunkter pr bane
+    // Vi trenger alle bookingene, banene og tidspunktene:
+    const bookings = useSelector((state: RootState) => state.bookings.bookings);
+    const courts = [1, 2, 3, 4, 5, 6, 7, 8]; // 8 baner
+    const allTimeslots = Array.from({ length: 14 }, (_, i) => `${8 + i}:00`); // 14 timeslots fra kl 8-21
+
+    // Lager oversikt over opptatte timeslots
+    const bookedTimeslots = bookings.filter(
+        (time) => time.date === selectedDate
+    );
+
+    // Funksjon for å sjekke ledige timeslots for hver bane
+    const availableTimeslots = (courtId: number) => {
+        const bookedTimes = bookedTimeslots
+            .filter((time) => time.courtId)
+            .map((time) => time.timeslot);
+
+        return allTimeslots.filter((time) => !bookedTimes.includes(time));
+    };
 
     return <div>BookingCalendar</div>;
 };
